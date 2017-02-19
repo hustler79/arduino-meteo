2dbf026637d5d9c2a544a9c464053809a6ba833d
Martin R <hustler@op.pl>
2017-02-19 19:46:12 +0100

    Final version

--- meteo.ino
+++ meteo.ino
@@ -1,4 +1,4 @@
-#include <Wire.h>
+#include <Wire.h> // for I2C operation
 #include <stdint.h>
 #include <SparkFunBME280.h>
 
@@ -9,6 +9,7 @@ BME280 mySensor;
 void setup() {
   //For I2C, enable the following and disable the SPI section
   mySensor.settings.commInterface = I2C_MODE;
+  //specify I2C address.  Can be 0x77(default) or 0x76
   mySensor.settings.I2CAddress = 0x77;
 
   //***Operation settings*****************************//
@@ -17,7 +18,7 @@ void setup() {
   //  0, Sleep mode
   //  1 or 2, Forced mode
   //  3, Normal mode
-  mySensor.settings.runMode = 3; //Forced mode
+  mySensor.settings.runMode = 3; //Normal mode
 
   //tStandby can be:
   //  0, 0.5ms
@@ -52,88 +53,13 @@ void setup() {
   //  0, skipped
   //  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
   mySensor.settings.humidOverSample = 1;
-  delay(10);  //Make sure sensor had enough time to turn on. BME280 requires 2ms to start up.
 
   Serial.begin(57600);
   Serial.print("Starting BME280... result of .begin(): 0x");
+  
   //Calling .begin() causes the settings to be loaded
+  delay(10);  //Make sure sensor had enough time to turn on. BME280 requires 2ms to start up.
   Serial.println(mySensor.begin(), HEX);
-
-  Serial.print("Displaying ID, reset and ctrl regs\n");
-  
-  Serial.print("ID(0xD0): 0x");
-  Serial.println(mySensor.readRegister(BME280_CHIP_ID_REG), HEX);
-  Serial.print("Reset register(0xE0): 0x");
-  Serial.println(mySensor.readRegister(BME280_RST_REG), HEX);
-  Serial.print("ctrl_meas(0xF4): 0x");
-  Serial.println(mySensor.readRegister(BME280_CTRL_MEAS_REG), HEX);
-  Serial.print("ctrl_hum(0xF2): 0x");
-  Serial.println(mySensor.readRegister(BME280_CTRL_HUMIDITY_REG), HEX);
-
-  Serial.print("\n\n");
-
-  Serial.print("Displaying all regs\n");
-  uint8_t memCounter = 0x80;
-  uint8_t tempReadData;
-  for(int rowi = 8; rowi < 16; rowi++ )
-  {
-    Serial.print("0x");
-    Serial.print(rowi, HEX);
-    Serial.print("0:");
-    for(int coli = 0; coli < 16; coli++ )
-    {
-      tempReadData = mySensor.readRegister(memCounter);
-      Serial.print((tempReadData >> 4) & 0x0F, HEX);//Print first hex nibble
-      Serial.print(tempReadData & 0x0F, HEX);//Print second hex nibble
-      Serial.print(" ");
-      memCounter++;
-    }
-    Serial.print("\n");
-  }
-  
-  
-  Serial.print("\n\n");
-  
-  Serial.print("Displaying concatenated calibration words\n");
-  Serial.print("dig_T1, uint16: ");
-  Serial.println(mySensor.calibration.dig_T1);
-  Serial.print("dig_T2, int16: ");
-  Serial.println(mySensor.calibration.dig_T2);
-  Serial.print("dig_T3, int16: ");
-  Serial.println(mySensor.calibration.dig_T3);
-  
-  Serial.print("dig_P1, uint16: ");
-  Serial.println(mySensor.calibration.dig_P1);
-  Serial.print("dig_P2, int16: ");
-  Serial.println(mySensor.calibration.dig_P2);
-  Serial.print("dig_P3, int16: ");
-  Serial.println(mySensor.calibration.dig_P3);
-  Serial.print("dig_P4, int16: ");
-  Serial.println(mySensor.calibration.dig_P4);
-  Serial.print("dig_P5, int16: ");
-  Serial.println(mySensor.calibration.dig_P5);
-  Serial.print("dig_P6, int16: ");
-  Serial.println(mySensor.calibration.dig_P6);
-  Serial.print("dig_P7, int16: ");
-  Serial.println(mySensor.calibration.dig_P7);
-  Serial.print("dig_P8, int16: ");
-  Serial.println(mySensor.calibration.dig_P8);
-  Serial.print("dig_P9, int16: ");
-  Serial.println(mySensor.calibration.dig_P9);
-  
-  Serial.print("dig_H1, uint8: ");
-  Serial.println(mySensor.calibration.dig_H1);
-  Serial.print("dig_H2, int16: ");
-  Serial.println(mySensor.calibration.dig_H2);
-  Serial.print("dig_H3, uint8: ");
-  Serial.println(mySensor.calibration.dig_H3);
-  Serial.print("dig_H4, int16: ");
-  Serial.println(mySensor.calibration.dig_H4);
-  Serial.print("dig_H5, int16: ");
-  Serial.println(mySensor.calibration.dig_H5);
-  Serial.print("dig_H6, uint8: ");
-  Serial.println(mySensor.calibration.dig_H6);
-    
   Serial.println();
 }
 
